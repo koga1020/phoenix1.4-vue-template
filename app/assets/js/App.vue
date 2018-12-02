ac887a72a4f61259fb0d5e212c381f329cba9552
koga1020 <koga.shozo1020@gmail.com>
2018-12-02 17:00:34 +0900

    update app.js, add App.vue

---
+++ app/assets/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+<div>タイトル: {{ title }}</div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      title: 'This is App.vue!!'
+    }
+  }
+}
+</script>
